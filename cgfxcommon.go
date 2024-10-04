24750c3d9a76d6b94ebd863ffed5a571d08f31a4
jurgen.kluft <jurgen.kluft@gmail.com>
2024-10-04 14:28:16 +0800

    * cgfx common
    * ccode update, can now determine which test_main.cpp to generate

--- cgfxcommon.go
+++ cgfxcommon.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/jurgen-kluft/ccode/ccode-base"
-	c "github.com/jurgen-kluft/cgfxcommon/package"
+	cpkg "github.com/jurgen-kluft/cgfxcommon/package"
 )
 
 func main() {
-	ccode.Init()
-	ccode.GenerateFiles()
-	ccode.Generate(c.GetPackage())
+	if ccode.Init() {
+		pkg := cpkg.GetPackage()
+		ccode.GenerateFiles(pkg)
+		ccode.Generate(pkg)
+	}
 }
